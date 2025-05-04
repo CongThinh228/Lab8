f32ca023f4a6fc34f1feca8c09c57ba28f902b9a
nhatminh205-real <minhlt2803@gmail.com>
2025-05-04 22:41:14 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên của bạn: ");
+        String name = scanner.nextLine();
+        System.out.println("Xin chào, " + name + "!");
+        scanner.close();
+    }
+}
